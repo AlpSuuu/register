61f27cef411f7bcb108c42e6aa5385d34c1851b8
AlpSuuu <67225902+AlpSuuu@users.noreply.github.com>
2021-04-07 18:33:38 +0300

    Update unregister.js

--- register/Commands/unregister.js
+++ register/Commands/unregister.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'unregister',
     aliases: ['kayıtsız'],
     async execute(client, message , args) {
-     if(!message.member.isAuthorized() && message.member.isAdministrator()) return message.sent("yeterli yetkin yok.")
+     if(!message.member.isAuthorized() && !message.member.isAdministrator()) return message.sent("yeterli yetkin yok.")
      let kişi = message.mentions.members.first() || message.guild.members.cache.get(args[0])   
      if(!args[0] || !kişi) return message.sent("Lütfen unregistere atılacak kişiyi belirtiniz.")
      let mention = message.guild.members.cache.get(kişi.id)
