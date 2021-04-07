3bfb1c436cca44a6a6688be0a30dcfb6c8381ece
AlpSuuu <67225902+AlpSuuu@users.noreply.github.com>
2021-04-07 18:33:04 +0300

    Update registerStatics.js

--- register/Commands/registerStatics.js
+++ register/Commands/registerStatics.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'kayıt-istatistik',
     aliases: ['bilgi'],
     async execute(client, message , args) {
-     if(!message.member.isAuthorized() && message.member.isAdministrator()) return message.sent("yeterli yetkin yok.")
+     if(!message.member.isAuthorized() && !message.member.isAdministrator()) return message.sent("yeterli yetkin yok.")
      let kişi = message.mentions.members.first() || message.guild.members.cache.get(args[0] || message.author.id)   
      if(!kişi) return message.sent("Lütfen istatistiklerine bakılacak kişiyi belirtiniz.")
      let mention = message.guild.members.cache.get(kişi.id)
