de920a4142d55613f08cefadebf8010f69d2a336
AlpSuuu <67225902+AlpSuuu@users.noreply.github.com>
2021-04-07 18:17:57 +0300

    Update badges.js

--- register/Commands/badges.js
+++ register/Commands/badges.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'badges',
     aliases: ['rozetler'],
     async execute(client, message , args) {
-     if(!message.member.isAuthorized() && message.member.isAdministrator()) return message.sent("Kayıt yapmak için yeterli yetkin yok.")
+     if(!message.member.isAuthorized() && !message.member.isAdministrator()) return message.sent("Kayıt yapmak için yeterli yetkin yok.")
      let kişi = message.mentions.members.first() || message.guild.members.cache.get(args[0] ? args[0] : message.author.id)   
      let mention = message.guild.members.cache.get(kişi.id)
      if(!mention) return message.sent("Belirttiğiniz Kişiyi bulamıyorum")
