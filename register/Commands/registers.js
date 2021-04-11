0103044f3186ee1b3eb8e5340ac5110d5326c018
AlpSuuu <67225902+AlpSuuu@users.noreply.github.com>
2021-04-11 13:37:07 +0300

    Update registers.js

--- register/Commands/registers.js
+++ register/Commands/registers.js
@@ -1,15 +1,13 @@
-
-const Discord = require("discord.js") 
 module.exports = {
-  name: "kayıtlar",
-  aliases: null,
-  async execute (client ,message , args) {
-  if(!message.member.isAuthorized() && !message.member.isAdministrator()) return message.sent("yeterli yetkin yok.")
-  let kişi = message.mentions.members.first() || message.guild.members.cache.get(args[0])   
-  let mention = message.guild.members.cache.get(kişi.id)
-  if(!args[0] || !kişi) return message.sent("Lütfen verilerine bakılacak kişiyi belirtiniz.")
-  if(!mention) return message.sent("Belirttiğiniz Kişiyi bulamıyorum")
-  message.member.registers(message)
-  await message.react(bot.config.onaylandı)
+   name: "kayıtlar",
+   aliases: null,
+   async execute (client ,message , args) {
+   if(!message.member.isAuthorized() && !message.member.isAdministrator()) return message.sent("yeterli yetkin yok.")
+   let kişi = message.mentions.members.first() || message.guild.members.cache.get(args[0])   
+   if(!args[0] || !kişi) return message.sent("Lütfen verilerine bakılacak kişiyi belirtiniz.")
+   let mention = message.guild.members.cache.get(kişi.id)
+   if(!mention) return message.sent("Belirttiğiniz Kişiyi bulamıyorum")
+   mention.registers(message)
+   await message.react(bot.config.onaylandı)
  }
 }
